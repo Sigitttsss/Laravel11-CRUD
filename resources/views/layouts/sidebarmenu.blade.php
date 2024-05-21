ef7fe69e735795d721ed2f4573cc8fffc14bc6a8
Sigit <sugiantorosigit3@gmail.com>
2024-05-21 20:28:54 +0700

    penambahan menu side bar

--- resources/views/layouts/sidebarmenu.blade.php
+++ resources/views/layouts/sidebarmenu.blade.php
@@ -10,12 +10,48 @@
             </a>
             <ul class="nav nav-treeview">
               <li class="nav-item">
-                <a href="{{ route('dashboard') }}" class="nav-link active">
+                <a href="{{ route('dashboard') }}" class="nav-link">
                   <i class="far fa-circle nav-icon"></i>
                   <p>Dashboard</p>
                 </a>
               </li>
             </ul>
           </li>
-    </ul>
+          <li class="nav-item">
+            <a href="" class="nav-link">
+              <i class="nav-icon fas fa-th"></i>
+              <p>
+                Penjualan
+              </p>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a href="" class="nav-link">
+              <i class="nav-icon nav-icon fas fa-book"></i>
+              <p>
+                Laporan
+              </p>
+            </a>
+          </li>
+          <div class="user-panel mt-2 pb-3 mb-2 d-flex"></div>
+          <li class="nav-item">
+            <a href="{{ route('profile.edit') }}" class="nav-link">
+              <i class="nav-icon nav-icon fas fa-male"></i>
+              <p>
+               Profile
+              </p>
+            </a>
+          </li>
+          <li class="nav-item">
+            <form method="POST" action="{{ route('logout') }}">
+                @csrf
+            <a href="route('logout')" onclick="event.preventDefault(); this.closest('form').submit();"class="nav-link">
+              <i class="nav-icon nav-icon fas fa-sign-out-alt"></i>
+              <p>
+               Log Out
+              </p>
+            </a>
+        </form>
+          </li>
+        </ul>
 </nav>
